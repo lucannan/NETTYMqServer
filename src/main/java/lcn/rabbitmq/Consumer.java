a9c4d1af1375a3e36625424933a39f56c85b7273
samuellcn <812481699@qq.com>
2016-05-23 10:56:33 +0800

    NettyMqServer

--- src/main/java/lcn/rabbitmq/Consumer.java
+++ src/main/java/lcn/rabbitmq/Consumer.java
@@ -3,12 +3,12 @@ package lcn.rabbitmq;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
 
 /**
  * Created by LCN on 2016/5/23.
  */
-public class Producer {
-
+public class Consumer {
     private final static String QUEUE_NAME = "hello";
 
     public static void main(String[] argv) throws Exception {
@@ -19,11 +19,15 @@ public class Producer {
         Channel channel = connection.createChannel();
 
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        String message = "Hello World!";
-        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-        System.out.println(" [x] Sent '" + message + "'");
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-        channel.close();
-        connection.close();
+        QueueingConsumer consumer = new QueueingConsumer(channel);
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+
+        while (true) {
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            String message = new String(delivery.getBody());
+            System.out.println(" [x] Received '" + message + "'");
+        }
     }
 }
